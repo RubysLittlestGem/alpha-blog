f5a4c2caf9706a8fbebd1308404f355b650e1b36
RubysLittlestGem <RubysLittlestGem@gmail.com>
2022-09-19 21:25:10 -0700

    add edit and update functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,41 @@
 class ArticlesController < ApplicationController
 
-    def show
-        @article = Article.find(params[:id])
+  def show
+    @article = Article.find(params[:id])
     end
 
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+   
+    if @article.save
+      puts flash[:notice] = "Successfully created article."  
+    redirect_to @article
+   else
+    render :new, status: :unprocessable_entity
+   end
+
     end
+
+    def update
+      @article = Article.find(params[:id])
+     if @article.update(params.require(:article).permit(:title, :description))
+      puts flash[:notice] = "Successfully updated article."
+      redirect_to @article
+      else
+      render :edit, status: :unprocessable_entity
+      
+    end
+  end
 end
